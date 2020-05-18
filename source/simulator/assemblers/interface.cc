4225f6397edd17f567c5fb324f6e7fa11134945d
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2020-05-18 10:00:32 -0700

    Make prefactor more accurate

--- source/simulator/assemblers/interface.cc
+++ source/simulator/assemblers/interface.cc
@@ -535,14 +535,9 @@ namespace aspect
 
       std::vector<double> prefactors(scratch.material_model_inputs.n_evaluation_points(), 1.0);
 
-      const double max_density = *std::max_element(scratch.material_model_outputs.densities.begin(),
-                                                   scratch.material_model_outputs.densities.end());
-      const double max_cp = *std::max_element(scratch.material_model_outputs.specific_heat.begin(),
-                                              scratch.material_model_outputs.specific_heat.end());
-
       if (scratch.advection_field->is_temperature())
         for (unsigned int i=0; i<prefactors.size(); ++i)
-          prefactors[i] = max_density * max_cp;
+          prefactors[i] = scratch.material_model_outputs.densities[i] * scratch.material_model_outputs.specific_heat[i];
 
       return prefactors;
     }
